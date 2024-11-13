f01fed1e9b19b1fd27d84c485eb79949781407a3
Wan Muzaffar <wanmuz86@gmail.com>
2024-11-13 09:42:22 +0700

    add form validator

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-reactive-form',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './reactive-form.component.html',
   styleUrl: './reactive-form.component.css'
 })
@@ -13,8 +14,8 @@ export class ReactiveFormComponent {
   // Defining the form element and validation rules for the Form
 
  productForm = new FormGroup({
-  name: new FormControl('',{nonNullable:true}),
-  price:  new FormControl(0, {nonNullable:true})
+  name: new FormControl('',{nonNullable:true, validators:[Validators.required, Validators.maxLength(255), Validators.minLength(10)]}),
+  price:  new FormControl(0, {nonNullable:true, validators:[Validators.min(1), Validators.required]})
  });
 
  // Getter form the value of the form 
